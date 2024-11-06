ad08dfe4882d258dc9f8ad4204df83f3ab2b0d3b
ramenha0141 <ramenhaoishii@gmail.com>
2024-11-06 16:59:01 +0900

    format

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,32 +1,36 @@
-import type { MetaFunction } from "@remix-run/cloudflare";
-import { useState } from "react";
+import type { MetaFunction } from '@remix-run/cloudflare';
+import { useState } from 'react';
 
 export const meta: MetaFunction = () => {
-  return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
-  ];
+	return [
+		{ title: 'New Remix App' },
+		{ name: 'description', content: 'Welcome to Remix!' },
+	];
 };
 
 export default function Index() {
-  const [userId, setUserId] = useState<string | null>(null);
+	const [userId, setUserId] = useState<string | null>(null);
 
-  const handleLogin = async () => {
-    console.log("login");
-    /*
+	const handleLogin = async () => {
+		console.log('login');
+		/*
     const response = await signIn.oauth2({
       providerId: "amazon-sp-api",
       callbackURL: "/",
     });
     console.log(response);*/
-  };
+	};
 
-  return (
-    <div className="flex h-screen items-center justify-center">
-      <button onClick={handleLogin} className="px-4 py-2 bg-blue-500 text-white rounded">ログイン</button>
+	return (
+		<div className='flex h-screen items-center justify-center'>
+			<button
+				onClick={handleLogin}
+				className='rounded bg-blue-500 px-4 py-2 text-white'
+			>
+				ログイン
+			</button>
 
-      {userId && <p>ログイン中: {userId}</p>}
-
-    </div>
-  );
+			{userId && <p>ログイン中: {userId}</p>}
+		</div>
+	);
 }
