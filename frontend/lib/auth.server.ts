b7829b94099eabb84ad2dcb9d1c25a2dc88de2b7
henohenon <takeru.hawk.son@gmail.com>
2024-11-08 11:31:52 +0900

    change redirectURI

--- frontend/lib/auth.server.ts
+++ frontend/lib/auth.server.ts
@@ -31,6 +31,7 @@ export const auth = betterAuth({
 					scopes: ['profile'],
 					tokenUrl: 'https://api.amazon.com/auth/o2/token',
 					userInfoUrl: 'https://api.amazon.com/user/profile',
+					redirectURI: 'https://seller-kanrikun.pages.dev/dashboad',
 					pkce: true,
 				},
 			],
