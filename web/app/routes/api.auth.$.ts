437e0931f917a840d7acdc26ac309a178ad9b712
ramenha0141 <ramenhaoishii@gmail.com>
2024-11-11 09:58:06 +0900

    spapi

--- web/app/routes/api.auth.$.ts
+++ web/app/routes/api.auth.$.ts
@@ -3,6 +3,18 @@ import type { ActionFunctionArgs, LoaderFunctionArgs } from '@remix-run/node';
 import { auth } from '~/lib/auth.server';
 
 export async function loader({ request }: LoaderFunctionArgs) {
+	if (
+		new URL(request.url).pathname === '/api/auth/oauth2/callback/seller-central'
+	) {
+		const url = new URL(request.url);
+
+		if (url.searchParams.has('spapi_oauth_code')) {
+			url.searchParams.set('code', url.searchParams.get('spapi_oauth_code')!);
+			url.searchParams.delete('spapi_oauth_code');
+		}
+
+		return auth.handler(new Request(url, request));
+	}
 	return auth.handler(request);
 }
 
