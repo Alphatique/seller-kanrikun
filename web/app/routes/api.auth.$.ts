98d24c4e4c83ad95a26abf421ef81a6b8b4420f1
ramenha0141 <ramenhaoishii@gmail.com>
2024-11-11 10:15:41 +0900

    spapi

--- web/app/routes/api.auth.$.ts
+++ web/app/routes/api.auth.$.ts
@@ -6,14 +6,20 @@ export async function loader({ request }: LoaderFunctionArgs) {
 	if (
 		new URL(request.url).pathname === '/api/auth/oauth2/callback/seller-central'
 	) {
-		const url = new URL(request.url);
+		const requestUrl = new URL(request.url);
+		const url = new URL(requestUrl.pathname);
 
-		if (url.searchParams.has('spapi_oauth_code')) {
-			url.searchParams.set('code', url.searchParams.get('spapi_oauth_code')!);
-			url.searchParams.delete('spapi_oauth_code');
+		if (requestUrl.searchParams.has('spapi_oauth_code')) {
+			url.searchParams.set(
+				'code',
+				requestUrl.searchParams.get('spapi_oauth_code')!,
+			);
+		}
+		if (requestUrl.searchParams.has('state')) {
+			url.searchParams.set('state', requestUrl.searchParams.get('state')!);
 		}
 
-		return auth.handler(new Request(url, request));
+		return auth.handler(new Request(requestUrl, request));
 	}
 	return auth.handler(request);
 }
