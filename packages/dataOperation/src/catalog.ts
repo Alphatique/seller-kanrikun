0d2e68dcb08bdb9b7caa870b9dda561999165fab
henohenon <takeru.hawk.son@gmail.com>
2024-11-16 09:36:23 +0900

    add dataoperation

---
+++ packages/dataOperation/src/catalog.ts
@@ -0,0 +1,49 @@
+import { account } from '@seller-kanrikun/db/schema';
+import { eq } from 'drizzle-orm';
+import { Hono } from 'hono';
+import type { CatalogItemsResponse, MyHonoInitializer } from '~/types';
+import { updateAccessToken } from './account';
+
+const app = new Hono<MyHonoInitializer>();
+
+app.get('/', async c => {
+	const itemList = [];
+
+	const db = c.get('DB');
+
+	const accounts = await db
+		.select()
+		.from(account)
+		.where(eq(account.providerId, 'seller-central'))
+		.all();
+
+	const eachAccount = accounts[0];
+
+	await updateAccessToken(
+		db,
+		eachAccount,
+		c.env.SP_API_CLIENT_ID,
+		c.env.SP_API_CLIENT_SECRET,
+	);
+
+	const response = await fetch(
+		'https://sellingpartnerapi-fe.amazon.com/catalog/2022-04-01/items?identifiersType=ASIN&identifiers=B06ZXXQGZ8&marketplaceIds=A1VC38T7YXB528',
+		{
+			method: 'GET',
+			headers: {
+				'x-amz-access-token': eachAccount.accessToken!,
+			},
+		},
+	);
+
+	const responseData: CatalogItemsResponse = await response.json();
+
+	console.log(responseData);
+	console.log(responseData.items[0]);
+	console.log(responseData.items[0].summaries);
+	console.log(responseData.items[0].summaries[0].browseClassification);
+
+	return new Response('ok');
+});
+
+export default app;
