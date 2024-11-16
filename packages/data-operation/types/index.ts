caa570a6a4bed05bd98749a3175c064302b96f62
henohenon <takeru.hawk.son@gmail.com>
2024-11-16 09:47:35 +0900

    change name and add tsconfig, package.json

--- packages/data-operation/types/index.ts
+++ packages/data-operation/types/index.ts
@@ -11,6 +11,17 @@ export type CustomVariables = {
 	DB: ClientType;
 };
 
+interface CloudflareBindings {
+	AMAZON_CLIENT_ID: string;
+	AMAZON_CLIENT_SECRET: string;
+	SP_API_APPLICATION_ID: string;
+	SP_API_CLIENT_ID: string;
+	SP_API_CLIENT_SECRET: string;
+	TURSO_CONNECTION_URL: string;
+	TURSO_AUTH_TOKEN: string;
+	MY_WEB_ORIGIN: string;
+}
+
 export type MyHonoInitializer = {
 	Bindings: CloudflareBindings;
 	Variables: CustomVariables;
