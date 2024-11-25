1915141eb86da9fb41e0bd53d9d0c61ed14f4179
henohenon <takeru.hawk.son@gmail.com>
2024-11-25 22:45:52 +0900

    format to json

--- api/src/index.ts
+++ api/src/index.ts
@@ -19,8 +19,7 @@ import {
 } from '@seller-kanrikun/data-operation';
 import type { SettlementReportsResponse } from '@seller-kanrikun/data-operation/types';
 import { createClient } from '@seller-kanrikun/db';
-
-import { parquet } from '@seller-kanrikun/data-operation';
+import { z } from 'zod';
 
 export default {
 	async fetch(request, env, ctx): Promise<Response> {
@@ -62,30 +61,120 @@ export default {
 		}
 	},
 	async scheduled(event, env, ctx): Promise<void> {
+		// dbの接続
 		const db = await createClient({
 			url: env.TURSO_CONNECTION_URL,
 			authToken: env.TURSO_AUTH_TOKEN,
 		});
+
+		// セラーのアカウントの全取得
 		const accounts = await getAccountsByProviderId(db, 'seller-central');
 
 		for (const account of accounts) {
+			// アクセストークンの更新
 			await updateAccessToken(
 				db,
 				account,
 				env.SP_API_CLIENT_ID,
 				env.SP_API_CLIENT_SECRET,
 			);
+			// レポート一覧の取得
 			const reports: SettlementReportsResponse = await getSettlementReports(
 				account.accessToken!,
 			);
+			// レポートドキュメントの取得
 			const document: string = await getReportDocument(
 				reports.reports[0].reportDocumentId,
 				account.accessToken!,
 			);
-			//console.log(document);
-			await parquet(document);
+			const json = csvToJson(document);
+			console.log(json);
+
+			//await env.MY_BUCKET.put('', json.toString());
 		}
 
 		console.log(accounts);
 	},
 } satisfies ExportedHandler<Env>;
+const ReportDocumentSchema = z.object({
+	'settlement-id': z.string().default(''),
+	'settlement-start-date': z
+		.string()
+		.optional()
+		.transform(val => (val ? new Date(val) : '')),
+	'settlement-end-date': z
+		.string()
+		.optional()
+		.transform(val => (val ? new Date(val) : '')),
+	'deposit-date': z
+		.string()
+		.optional()
+		.transform(val => (val ? new Date(val) : '')),
+	'total-amount': z.string().default(''),
+	currency: z.string().default(''),
+	'transaction-type': z.string().default(''),
+	'order-id': z.string().default(''),
+	'merchant-order-id': z.string().default(''),
+	'adjustment-id': z.string().default(''),
+	'shipment-id': z.string().default(''),
+	'marketplace-name': z.string().default(''),
+	'shipment-fee-type': z.string().default(''),
+	'shipment-fee-amount': z.string().default(''),
+	'order-fee-type': z.string().default(''),
+	'order-fee-amount': z.string().default(''),
+	'fulfillment-id': z.string().default(''),
+	'posted-date': z
+		.string()
+		.optional()
+		.transform(val => (val ? new Date(val) : '')),
+	'order-item-code': z.string().default(''),
+	'merchant-order-item-id': z.string().default(''),
+	'merchant-adjustment-item-id': z.string().default(''),
+	sku: z.string().default(''),
+	'quantity-purchased': z.string().default(''),
+	'price-type': z.string().default(''),
+	'price-amount': z.string().default(''),
+	'item-related-fee-type': z.string().default(''),
+	'item-related-fee-amount': z
+		.string()
+		.optional()
+		.transform(val => {
+			if (val) {
+				const num = Number.parseFloat(val);
+				return Number.isNaN(num) ? '' : num;
+			}
+			return '';
+		}),
+	'misc-fee-amount': z.string().default(''),
+	'other-fee-amount': z.string().default(''),
+	'other-fee-reason-description': z.string().default(''),
+	'promotion-id': z.string().default(''),
+	'promotion-type': z.string().default(''),
+	'promotion-amount': z.string().default(''),
+	'direct-payment-type': z.string().default(''),
+	'direct-payment-amount': z.string().default(''),
+	'other-amount': z.string().default(''),
+});
+
+type ReportDocumentJson = z.infer<typeof ReportDocumentSchema>;
+
+const csvToJson = (csv: string): ReportDocumentJson[] => {
+	// 改行で分割
+	const lines = csv.split('\n');
+	const headers = lines[0].split('\t');
+
+	// ヘッダーをキーにしてオブジェクトに変換
+	return lines.slice(1).map(line => {
+		// タブで分割
+		const values = line.split('\t');
+		const row: Record<string, string> = {};
+
+		// ヘッダーと値をセット
+		headers.forEach((header, index) => {
+			row[header] = values[index] || '';
+		});
+
+		// zodでパース
+		return ReportDocumentSchema.parse(row);
+	});
+};
