b5d58645efcf8ac24f81c681c622f38d3c3c29ae
ramenha0141 <ramenhaoishii@gmail.com>
2024-11-28 13:41:47 +0900

    baselime

--- web/functions/[[path]].ts
+++ web/functions/[[path]].ts
@@ -1,7 +1,23 @@
+import { BaselimeLogger } from '@baselime/edge-logger';
 import { createPagesFunctionHandler } from '@remix-run/cloudflare-pages';
 
 // @ts-ignore
 import * as build from '../build/server';
 
 // @ts-ignore
-export const onRequest = createPagesFunctionHandler({ build });
+const handler = createPagesFunctionHandler({ build });
+
+export function onRequest(context: EventContext<Env, string, unknown>) {
+	const url = new URL(context.request.url);
+	const logger = new BaselimeLogger({
+		service: 'seller-kanrikun',
+		namespace: `${context.request.method} ${url.hostname}${url.pathname}`,
+		apiKey: context.env.BASELIME_API_KEY,
+		ctx: context,
+		isLocalDev: !context.env.BASELIME_API_KEY,
+	});
+
+	context.waitUntil(logger.flush());
+
+	return handler(context);
+}
