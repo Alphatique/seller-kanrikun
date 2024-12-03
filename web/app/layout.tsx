546236dbba10ecc748e96bf3a1992cd87ed9b9ac
ramenha0141 <ramenhaoishii@gmail.com>
2024-12-03 16:50:08 +0900

    feat(web): add document metadata and icons

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -14,8 +14,35 @@ const geistMono = localFont({
 });
 
 export const metadata: Metadata = {
-	title: 'Seller Kanrikun',
-	description: '',
+	title: 'セラー管理君',
+	icons: [
+		{
+			rel: 'icon',
+			type: 'image/png',
+			url: '/favicon-96x96.png',
+		},
+		{
+			rel: 'icon',
+			type: 'image/svg+xml',
+			url: '/favicon.svg',
+		},
+		{
+			rel: 'shortcut icon',
+			url: '/favicon.ico',
+		},
+		{
+			rel: 'apple-touch-icon',
+			sizes: '180x180',
+			url: '/apple-touch-icon.png',
+		},
+		{
+			rel: 'manifest',
+			url: '/site.webmanifest',
+		},
+	],
+	appleWebApp: {
+		title: 'セラー管理君',
+	},
 };
 
 export default function RootLayout({ children }: LayoutProps) {
--- web/app/sign-in/layout.tsx
+++ web/app/sign-in/layout.tsx
@@ -1,3 +1,9 @@
+import type { Metadata } from 'next';
+
+export const metadata: Metadata = {
+	title: 'ログイン | セラー管理君',
+};
+
 export default function Layout({ children }: LayoutProps) {
 	return (
 		<div className='grid h-screen place-content-center bg-muted'>
