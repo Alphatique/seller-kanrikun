59cd8ed0aa6ff51674bd44659e5ff50b294cc003
ramenha0141 <ramenhaoishii@gmail.com>
2024-12-03 11:56:56 +0900

    fix(web): environment variables
    replace process.env.{key} with the value of the environment variable during the build phase

--- web/next.config.ts
+++ web/next.config.ts
@@ -1,5 +1,25 @@
+import { execSync } from 'node:child_process';
 import type { NextConfig } from 'next';
+import { DefinePlugin } from 'webpack';
 
-const nextConfig: NextConfig = {};
+const nextConfig: NextConfig = {
+	webpack: (config, { nextRuntime }) => {
+		if (nextRuntime === 'edge' || nextRuntime === 'nodejs') {
+			const env = JSON.parse(
+				execSync('bunx dotenvx get -f ../.env').toString(),
+			);
+
+			const defines: Record<string, string> = {};
+
+			for (const [key, value] of Object.entries(env)) {
+				defines[`process.env.${key}`] = JSON.stringify(value);
+			}
+
+			config.plugins.push(new DefinePlugin(defines));
+		}
+
+		return config;
+	},
+};
 
 export default nextConfig;
