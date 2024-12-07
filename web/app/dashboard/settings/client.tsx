4b2b228130ec3b8ae400724838bcebc91828cb20
ramenha0141 <ramenhaoishii@gmail.com>
2024-12-07 22:59:44 +0900

    feat: create passkey dialog

--- web/app/dashboard/settings/client.tsx
+++ web/app/dashboard/settings/client.tsx
@@ -1,11 +1,31 @@
 'use client';
 
-import { useRouter } from 'next/navigation';
+import { valibotResolver } from '@hookform/resolvers/valibot';
+import { useForm } from 'react-hook-form';
+import * as v from 'valibot';
 
 import { Button } from '@seller-kanrikun/ui/components/button';
 import { useConfirm } from '@seller-kanrikun/ui/confirm-dialog';
 
-import { passkey } from '@seller-kanrikun/auth/client';
+import { passkey, signOut } from '@seller-kanrikun/auth/client';
+import {
+	Dialog,
+	DialogContent,
+	DialogDescription,
+	DialogHeader,
+	DialogTitle,
+	DialogTrigger,
+} from '@seller-kanrikun/ui/components/dialog';
+import {
+	Form,
+	FormControl,
+	FormField,
+	FormItem,
+	FormLabel,
+	FormMessage,
+} from '@seller-kanrikun/ui/components/form';
+import { Input } from '@seller-kanrikun/ui/components/input';
+import { useRouter } from 'next/navigation';
 import { signOutAllSessions } from './actions';
 
 export function SignOutAllSessionButton() {
@@ -27,15 +47,92 @@ export function SignOutAllSessionButton() {
 }
 
 export function CreatePasskeyButton() {
+	return (
+		<Dialog>
+			<DialogTrigger asChild>
+				<Button>パスキーを作成</Button>
+			</DialogTrigger>
+			<DialogContent>
+				<DialogHeader>
+					<DialogTitle>パスキーを作成</DialogTitle>
+					<DialogDescription>
+						名前を入力してパスキーを作成します
+					</DialogDescription>
+				</DialogHeader>
+				<CreatePasskeyForm />
+			</DialogContent>
+		</Dialog>
+	);
+}
+
+const createPasskeyFormSchema = v.object({
+	name: v.pipe(v.string(), v.minLength(1, '名前を入力してください')),
+});
+
+export function CreatePasskeyForm() {
 	const router = useRouter();
+	const confirm = useConfirm();
+	const form = useForm<v.InferInput<typeof createPasskeyFormSchema>>({
+		resolver: valibotResolver(createPasskeyFormSchema),
+		defaultValues: {
+			name: '',
+		},
+	});
 
-	async function onClick() {
-		const { error } = (await passkey.addPasskey())!;
+	async function onSubmit({
+		name,
+	}: v.InferInput<typeof createPasskeyFormSchema>) {
+		const result = await passkey.addPasskey({
+			name,
+		});
 
-		if (!error) {
+		if (result?.error) {
+			switch ((result.error as unknown as { code: string }).code) {
+				case 'SESSION_IS_NOT_FRESH': {
+					if (
+						await confirm({
+							title: 'セッションエラー',
+							description: 'パスキーを作成するにはサインインし直してください',
+							confirmText: 'サインアウト',
+						})
+					) {
+						await signOut();
+						router.push('/sign-in');
+					}
+
+					break;
+				}
+				default: {
+					await confirm({
+						title: 'エラー',
+						description: 'パスキーの作成に失敗しました',
+					});
+				}
+			}
+		} else {
 			router.refresh();
 		}
 	}
 
-	return <Button onClick={onClick}>パスキーを作成</Button>;
+	return (
+		<Form {...form}>
+			<form onSubmit={form.handleSubmit(onSubmit)} className='grid gap-4'>
+				<FormField
+					control={form.control}
+					name='name'
+					render={({ field }) => (
+						<FormItem>
+							<FormLabel>名前</FormLabel>
+							<FormControl>
+								<Input {...field} />
+							</FormControl>
+							<FormMessage />
+						</FormItem>
+					)}
+				/>
+
+				<Button type='submit'>作成</Button>
+			</form>
+		</Form>
+	);
 }
