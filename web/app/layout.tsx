131d9eafe68d98f8d7a03f65c37839517c772775
ramenha0141 <ramenhaoishii@gmail.com>
2024-12-07 15:34:56 +0900

    refactor: remove font

--- web/app/layout.tsx
+++ web/app/layout.tsx
@@ -1,17 +1,4 @@
-import localFont from 'next/font/local';
-
 import './globals.css';
-
-const geistSans = localFont({
-	src: './fonts/GeistVF.woff',
-	variable: '--font-geist-sans',
-	weight: '100 900',
-});
-const geistMono = localFont({
-	src: './fonts/GeistMonoVF.woff',
-	variable: '--font-geist-mono',
-	weight: '100 900',
-});
 
 export const metadata: Metadata = {
 	title: 'セラー管理君',
@@ -48,11 +35,7 @@ export const metadata: Metadata = {
 export default function RootLayout({ children }: LayoutProps) {
 	return (
 		<html lang='ja'>
-			<body
-				className={`${geistSans.variable} ${geistMono.variable} antialiased`}
-			>
-				{children}
-			</body>
+			<body className='antialiased'>{children}</body>
 		</html>
 	);
 }
