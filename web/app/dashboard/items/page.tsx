b6d496e5ed94068359047d70d008ecd8d49b921f
henohenon <takeru.hawk.son@gmail.com>
2024-12-11 11:38:44 +0900

    feat: add items page

---
+++ web/app/dashboard/items/page.tsx
@@ -0,0 +1,14 @@
+import { Separator } from '@seller-kanrikun/ui/components/separator';
+
+export const metadata: Metadata = {
+	title: '商品別明細 | セラー管理君',
+};
+
+export default function Page() {
+	return (
+		<div className='grid gap-4'>
+			<h1 className='font-bold text-3xl'>商品別明細</h1>
+			<Separator />
+		</div>
+	);
+}
