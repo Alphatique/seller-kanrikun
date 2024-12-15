b818ce2d232ebdd06299bbdab1e76276b740285b
henohenon <66393463+henohenon@users.noreply.github.com>
2024-12-15 11:32:38 +0900

    Cron job (#17)
    * feat: add get reports cron
    
    * feat: add get reports cron
    
    * feat: add retry with next token
    
    * feat: get report document data
    
    * build: add s3 and papaparse, fflate packages
    
    * chore: add ignorewords
    
    * feat: add get and save all reports api

--- web/app/api/henoheno/route.ts
+++ web/app/api/henoheno/route.ts
@@ -1,8 +1,8 @@
 import { getInventoryData } from '@seller-kanrikun/api-fetch';
 
 export async function GET(request: Request) {
-	const accessToken = '';
-	await getInventoryData(accessToken);
+	const accountId = '';
+	await getInventoryData(accountId);
 	return new Response('henohenomoheji', {
 		headers: {
 			'Content-Type': 'text/plain; charset=utf-8',
