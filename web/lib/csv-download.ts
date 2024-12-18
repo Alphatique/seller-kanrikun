1fdb77c0dcedde3366e639ed6e9ef76d78323443
henohenon <takeru.hawk.son@gmail.com>
2024-12-18 10:50:14 +0900

    refactor: download func

--- web/lib/csv-download.ts
+++ web/lib/csv-download.ts
@@ -16,24 +16,17 @@ export function downloadCsv(
 	// Blobを作成
 	const blob = new Blob([csvContent], { type: 'text/csv;charset=utf-8;' });
 
-	// ダウンロードリンクを作成
-	const link = document.createElement('a');
-	link.href = URL.createObjectURL(blob);
-	link.download = fileName;
-	link.style.display = 'none';
-	document.body.appendChild(link);
-
-	// ダウンロードを実行
-	link.click();
-
-	// リンクを削除
-	document.body.removeChild(link);
+	downloadBlob(blob, fileName);
 }
 
 export function downloadStr(data: string, fileName: string): void {
 	// Blobを作成
 	const blob = new Blob([data], { type: 'text/plain;charset=utf-8;' });
 
+	downloadBlob(blob, fileName);
+}
+
+function downloadBlob(blob: Blob, fileName: string): void {
 	// ダウンロードリンクを作成
 	const link = document.createElement('a');
 	link.href = URL.createObjectURL(blob);
