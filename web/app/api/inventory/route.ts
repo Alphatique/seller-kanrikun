4d42109796b719f9da91aa845f6fcba16d47439f
henohenon <takeru.hawk.son@gmail.com>
2024-12-25 09:49:15 +0900

    refactor: basic get to slug

--- web/app/api/inventory/route.ts
+++
@@ -1,5 +0,0 @@
-import { getApi, inventorySummariesFileName } from '~/lib/r2';
-
-export async function GET(request: Request): Promise<Response> {
-	return getApi(request, inventorySummariesFileName);
-}
--- web/app/api/reports/route.ts
+++
@@ -1,5 +0,0 @@
-import { costPriceFileName, getApi } from '~/lib/r2';
-
-export async function GET(request: Request): Promise<Response> {
-	return getApi(request, costPriceFileName);
-}
