96e94d2b7241bd787b1bc3b671a5680e772bddbe
ramenha0141 <ramenhaoishii@gmail.com>
2024-12-25 17:54:55 +0900

    Revert "feat: debug catch all route segment"
    This reverts commit 0f5d0635d5888d7c7d0f24928e5583fd4219866a.

--- web/app/api/[...all]/route.ts
+++ web/app/api/[...all]/route.ts
@@ -4,10 +4,5 @@ import { app } from '~/api';
 
 const handler = handle(app);
 
-function _handler(req: Request) {
-	console.log(req.url);
-	return handler(req);
-}
-
-export const GET = _handler;
-export const POST = _handler;
+export const GET = handler;
+export const POST = handler;
