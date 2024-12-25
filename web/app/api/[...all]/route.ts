0f5d0635d5888d7c7d0f24928e5583fd4219866a
ramenha0141 <ramenhaoishii@gmail.com>
2024-12-25 17:24:04 +0900

    feat: debug catch all route segment

--- web/app/api/[...all]/route.ts
+++ web/app/api/[...all]/route.ts
@@ -4,5 +4,10 @@ import { app } from '~/api';
 
 const handler = handle(app);
 
-export const GET = handler;
-export const POST = handler;
+function _handler(req: Request) {
+	console.log(req.url);
+	return handler(req);
+}
+
+export const GET = _handler;
+export const POST = _handler;
