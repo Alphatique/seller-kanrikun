798e1967c69ee6b7a899485527ac2adeb9a53ba5
henohenon <takeru.hawk.son@gmail.com>
2024-12-31 14:01:27 +0900

    feat: add first inventory

--- web/lib/constants.ts
+++ web/lib/constants.ts
@@ -1,5 +1,6 @@
 export const R2_BUCKET_NAME = 'seller-kanrikun';
 export const JAPAN_MARKET_PLACE_ID = 'A1VC38T7YXB528';
+export const SELLER_API_BASE_URL = 'https://sellingpartnerapi-fe.amazon.com';
 export const FILE_NAMES = {
 	SETTLEMENT_REPORT: 'settlement-report.tsv.gz',
 	SALES_TRAFFIC: 'sales-traffic-report.tsv.gz',
--- web/server/first.ts
+++ web/server/first.ts
@@ -1,6 +1,7 @@
 import { Readable } from 'node:stream';
 import type { ReadableStream as WebReadableStream } from 'node:stream/web';
-import type { paths } from '@seller-kanrikun/sp-api/schema/reports';
+import type { paths as inventoryPaths } from '@seller-kanrikun/sp-api/schema/fba-inventory';
+import type { paths as reportsPaths } from '@seller-kanrikun/sp-api/schema/reports';
 import { Hono } from 'hono';
 import createApiClient from 'openapi-fetch';
 import type { Middleware } from 'openapi-fetch';
@@ -8,22 +9,30 @@ import Papa from 'papaparse';
 
 import { tsvObjToTsvGzip } from '@seller-kanrikun/data-operation/tsv-gzip';
 
-import { FILE_NAMES } from '~/lib/constants';
+import {
+	FILE_NAMES,
+	JAPAN_MARKET_PLACE_ID,
+	SELLER_API_BASE_URL,
+} from '~/lib/constants';
 import { putFile } from '~/lib/r2';
 
+import { InventorySummary } from '@seller-kanrikun/data-operation/types/inventory';
 import {
 	accessTokenMiddleware,
 	authMiddleware,
 	dbMiddleware,
 } from './middleware';
 
+const baseUrl = 'https://sellingpartnerapi-fe.amazon.com';
+
 export const app = new Hono()
 	.use(authMiddleware)
 	.get('/', accessTokenMiddleware, async c => {
 		const accessToken = c.var.spApiAccessToken;
 
-		const reportsApi = createApiClient<paths>({
-			baseUrl: '',
+		// レポートapi
+		const reportsApi = createApiClient<reportsPaths>({
+			baseUrl: SELLER_API_BASE_URL,
 		});
 
 		const tokenMiddleware: Middleware = {
@@ -147,5 +156,79 @@ export const app = new Hono()
 			const gzippedReports = tsvObjToTsvGzip(documentRows);
 			const userId = c.var.user.id;
 			putFile(userId, FILE_NAMES.SETTLEMENT_REPORT, gzippedReports);
+		}
+
+		// 以下inventoryの処理
+		const inventoryApi = createApiClient<inventoryPaths>({
+			baseUrl: SELLER_API_BASE_URL,
+		});
+
+		inventoryApi.use(tokenMiddleware);
+
+		const inventoryQueries = {
+			granularityType: 'Marketplace' as const,
+			granularityId: JAPAN_MARKET_PLACE_ID,
+			marketplaceIds: [JAPAN_MARKET_PLACE_ID],
+		};
+
+		// 一覧の取得
+		let inventoryResponse = await inventoryApi.GET(
+			'/fba/inventory/v1/summaries',
+			{
+				params: {
+					query: {
+						...inventoryQueries,
+					},
+				},
+			},
+		);
+
+		const summaries: object[] = [];
+		const summariesRows: Record<string, string>[] = [];
+		if (inventoryResponse.data !== undefined) {
+			// 今のデータを保存
+			const inventorySummaries =
+				inventoryResponse?.data?.payload?.inventorySummaries;
+			if (inventorySummaries) {
+				summaries.push(inventorySummaries);
+			}
+			// nextTokenがあるかぎり続けて取得
+			while (
+				inventoryResponse?.data?.pagination?.nextToken !== undefined
+			) {
+				const nextToken: string =
+					inventoryResponse.data.pagination.nextToken;
+				inventoryResponse = await inventoryApi.GET(
+					'/fba/inventory/v1/summaries',
+					{
+						params: {
+							query: {
+								...inventoryQueries,
+								nextToken: nextToken,
+							},
+						},
+					},
+				);
+
+				// レスポンスがある場合は追加
+				const inventorySummaries =
+					inventoryResponse?.data?.payload?.inventorySummaries;
+				if (inventorySummaries) {
+					summaries.push(inventorySummaries);
+				}
+			}
+
+			// 取得したデータをまとめる
+			for (const inventorySummary of summaries) {
+				for (const [key, value] of Object.entries(inventorySummary)) {
+					if (value === null) continue;
+					summariesRows.push({ [key]: value as string });
+				}
+			}
+
+			// tsvGzipに変換して保存
+			const gzipped = await tsvObjToTsvGzip(summariesRows);
+			const userId = c.var.user.id;
+			putFile(userId, FILE_NAMES.INVENTORY_SUMMARIES, gzipped);
 		}
 	});
