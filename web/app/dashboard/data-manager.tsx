679ec667fddaf67c8f1e334b0ca7eadeaac1ba1b
henohenon <takeru.hawk.son@gmail.com>
2025-01-03 10:49:33 +0900

    refactor: remove datamanager

--- web/app/dashboard/data-manager.tsx
+++
@@ -1,8 +0,0 @@
-import type { AsyncDuckDB, AsyncDuckDBConnection } from '@duckdb/duckdb-wasm';
-import { atomWithImmer } from 'jotai-immer';
-
-import { initDuckDB } from '~/lib/duckdb';
-
-class MyDataManager {}
-
-export const dataManagerAtom = atomWithImmer(new MyDataManager());
