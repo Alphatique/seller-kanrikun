01345458f9d9c7a89b28033c2c7987896ca820a5
henohenon <66393463+henohenon@users.noreply.github.com>
2025-01-31 10:10:04 +0900

    fix: add db middleware

--- web/server/init.ts
+++ web/server/init.ts
@@ -75,6 +75,7 @@ const salesTrafficReportLimit = 7;
 const settlementReportLimit = 15 - salesTrafficReportLimit;
 
 export const app = new Hono()
+	.use(dbMiddleware)
 	.use(authMiddleware)
 	.use(accessTokenMiddleware)
 	.get('/cost-price', async c => {
