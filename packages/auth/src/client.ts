8c69f3cc2e972799a7ee62c602c6711a8762ba93
ramenha0141 <ramenhaoishii@gmail.com>
2025-02-20 17:30:39 +0900

    fix: fixed an error caused by BASE_URL not being set

--- packages/auth/src/client.ts
+++ packages/auth/src/client.ts
@@ -2,6 +2,7 @@ import { genericOAuthClient, passkeyClient } from 'better-auth/client/plugins';
 import { createAuthClient } from 'better-auth/react';
 
 export const client = createAuthClient({
+	baseURL: process.env.SELLER_KANRIKUN_BASE_URL,
 	plugins: [genericOAuthClient(), passkeyClient()],
 });
 
